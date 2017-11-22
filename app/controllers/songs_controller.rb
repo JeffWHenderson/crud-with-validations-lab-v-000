8f8c48ce8b3ef850acb8ff816fbe698fc60abc12
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-22 15:21:24 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -16,7 +16,7 @@ class SongsController < ApplicationController
     @song = Song.new(song_params)
     if @song.valid?
       @song.save
-      redirect_to :show
+      redirect_to song_path(@song)
     else
       render :new
     end
