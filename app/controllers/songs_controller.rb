9b556edf64bb1f83364017e54d5bf9e3ba954f99
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-22 15:19:55 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,7 +13,9 @@ class SongsController < ApplicationController
   end
 
   def create
-    if @song.create(song_params)
+    @song = Song.new(song_params)
+    if @song.valid?
+      @song.save
       redirect :show
     else
       render :new
