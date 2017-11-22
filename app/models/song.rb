4f01527ee76fce110000bd1fe46ae59ce725a3d1
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-22 04:48:28 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,12 +2,8 @@ class Song < ActiveRecord::Base
   validates :title, presence: true
   validates :title, uniqueness: true #test passed but I should make this work more as intended
   validates :released, inclusion: { in: [ true, false ] }
-  # is invalid when the release year is in the future (FAILED - 16)
+
   validates :release_year, presence: true, if: :released
   validates :release_year, numericality: { less_than_or_equal_to: Date.today.year}, if: :released
-  # is invalid without release year when released is true (FAILED - 15)
-      # Optional if released is false
-      # Must not be blank if released is true
-      # Must be less than or equal to the current year
   validates :artist_name, presence: true
 end
