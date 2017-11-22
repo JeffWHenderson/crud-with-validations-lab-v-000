9ca841bf4b370bebc95251128bfe4e82cb6687f2
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-22 15:34:51 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -27,7 +27,7 @@ class SongsController < ApplicationController
   end
 
   def update
-    @song = song.find(params[:id])
+    @song = Song.find(params[:id])
     if @song.update(song_params)
       redirect_to song_path(@song)
     else
