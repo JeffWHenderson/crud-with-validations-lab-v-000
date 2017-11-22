a6bfbb2ec8f61d8cb0b0799064cb4fb0806dc7c7
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-22 04:22:14 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,5 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
-  validates :title, uniqueness: true
+  validates :title, uniqueness: true #test passed but I should make this work more as intended
+  validates :released, inclusion: { in: [ true, false ] }
 end
